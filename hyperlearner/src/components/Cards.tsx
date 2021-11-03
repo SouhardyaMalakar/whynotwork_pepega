2e825f25630bb0987a3817253923bb962dcd4be0
dreadarceus <dreadarceus@gmail.com>
2021-11-03 23:01:13 +0530

    yepw

--- hyperlearner/src/components/Cards.tsx
+++ hyperlearner/src/components/Cards.tsx
@@ -1,50 +1,53 @@
-import { Box } from '@chakra-ui/layout'
-import React from 'react'
-import Card_item from './Card_item'
+import React from "react";
+import { Box } from "@chakra-ui/layout";
+import Card_item from "./Card_item";
 
-function cards({articles}) {
+import { AllCoursesQuery } from "../generated/graphql";
+
+interface CardsProps {
+  articles: AllCoursesQuery["getAllCourses"];
+}
+
+export const Cards: React.FC<CardsProps> = ({ articles }) => {
   return (
     <Box
       width="100%"
-      display= "flex"
-      alignItems= "center"
+      display="flex"
+      alignItems="center"
       justifyContent="center"
       flexWrap="wrap"
       padding="50px"
     >
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      {articles.map((article) =>(
-      <Card_item article={article} key={article.id}/>
+      {articles.map((article) => (
+        <Card_item article={article} key={article.id} />
       ))}
-      
     </Box>
-  )
-}
-
-export default cards
+  );
+};
--- hyperlearner/src/pages/index.tsx
+++ hyperlearner/src/pages/index.tsx
@@ -1,33 +1,29 @@
-import Head from 'next/head'
-import Hero from '../components/Hero'
-import Cards from '../components/Cards'
-import { Box, Text } from '@chakra-ui/layout'
-import {  useAllCoursesQuery } from '../generated/graphql'
-import { useState } from 'react'
-
-
+import Head from "next/head";
+import Hero from "../components/Hero";
+import { Cards } from "../components/Cards";
+import { Box, Text } from "@chakra-ui/layout";
+import { useAllCoursesQuery } from "../generated/graphql";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [{ data }] = useAllCoursesQuery ();  
-  // if(data!==undefined){
-  //   const body = data;
-  //   console.log(body.getAllCourses);
-  // }
-   return (
+  const [{ data }] = useAllCoursesQuery();
+  const [arr, setArr] = useState([]);
+  useEffect(() => {
+    if (data) {
+      setArr(data.getAllCourses);
+    } else {
+      setArr([]);
+    }
+  });
+  return (
     <Box>
       <Head>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Hero/>
-      <Box
-        display= "flex"
-        alignItems= "center"
-        justifyContent="center"
-      >
-        <Cards articles ={data.getAllCourses}/>
+      <Hero />
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Cards articles={arr} />
       </Box>
     </Box>
-  )
+  );
 }
-
-
